a72995a0c32fdb2cdf9eb6831594e0787d038756
InJung Chung <mu29@yeoubi.net>
2017-11-08 15:22:57 +0900

    메인 화면 인터렉션을 구현한다

--- app/src/main/java/net/yeoubi/turntable/MainActivity.kt
+++ app/src/main/java/net/yeoubi/turntable/MainActivity.kt
@@ -1,23 +1,44 @@
 package net.yeoubi.turntable
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.yeoubi.turntable.utils.DimensionUtils
+import net.yeoubi.turntable.extensions.updateMargins
 
-class MainActivity : AppCompatActivity() {
 
-    private var contentY: Float = 0.0f
+class MainActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
+
+    private val musicAdapter = MusicAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        app_bar.addOnOffsetChangedListener { bar, offset ->
-            background_image.alpha = 1 - 0.8f * (-DimensionUtils.context(context = this).toDp(offset) / 184.0f)
-//            contentY = if (contentY == 0.0f) content_view.y else contentY
-//            content_view.y = contentY + offset * 0.7f
-        }
+        app_bar.addOnOffsetChangedListener(this)
+
+        setRecyclerView()
+    }
+
+    override fun onOffsetChanged(bar: AppBarLayout?, offset: Int) {
+        val dimension = DimensionUtils.context(this)
+        background_image.alpha = 1 - 0.8f * (-dimension.toDp(offset) / 184.0f)
+        content_view.updateMargins(8, (-32 - dimension.toDp(offset) / (200 / 32)).toInt(), 8, 0)
+    }
+
+    private fun setRecyclerView() {
+        musicAdapter.addItem(Music(index = 76229, title = "첫눈처럼 너에게 가겠다", artist = "에일리"))
+        musicAdapter.addItem(Music(index = 49082, title = "어디에도", artist = "MC THE MAX"))
+        musicAdapter.addItem(Music(index = 43918, title = "사진을 보다가", artist = "바이브"))
+        musicAdapter.addItem(Music(index = 76229, title = "첫눈처럼 너에게 가겠다", artist = "에일리"))
+        musicAdapter.addItem(Music(index = 49082, title = "어디에도", artist = "MC THE MAX"))
+        musicAdapter.addItem(Music(index = 43918, title = "사진을 보다가", artist = "바이브"))
+        musicAdapter.addItem(Music(index = 76229, title = "첫눈처럼 너에게 가겠다", artist = "에일리"))
+        musicAdapter.addItem(Music(index = 49082, title = "어디에도", artist = "MC THE MAX"))
+        musicAdapter.addItem(Music(index = 43918, title = "사진을 보다가", artist = "바이브"))
+        music_list.setHasFixedSize(true)
+        music_list.adapter = musicAdapter
     }
 }
---
+++ app/src/main/java/net/yeoubi/turntable/extensions/View.kt
@@ -0,0 +1,20 @@
+package net.yeoubi.turntable.extensions
+
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * InJung Chung
+ */
+
+fun View.updateMargins(left: Int, top: Int, right: Int, bottom: Int) {
+    val density = context.resources.displayMetrics.density
+    val params = this.layoutParams as ViewGroup.MarginLayoutParams
+    params.setMargins(
+        (left * density).toInt(),
+        (top * density).toInt(),
+        (right * density).toInt(),
+        (bottom * density).toInt()
+    )
+    this.requestLayout()
+}
