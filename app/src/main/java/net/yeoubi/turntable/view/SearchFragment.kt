43cab8502f5806732096575ecf02a8b92b2c4141
InJung Chung <mu29@yeoubi.net>
2017-11-09 16:57:00 +0900

    SearchFragment를 SearchActivity, MusicActivity에서 사용할 수 있도록 디자인한다

--- app/src/main/java/net/yeoubi/turntable/view/SearchFragment.kt
+++ app/src/main/java/net/yeoubi/turntable/view/SearchFragment.kt
@@ -1,24 +1,68 @@
 package net.yeoubi.turntable.view
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jakewharton.rxbinding2.widget.RxTextView
 
 import net.yeoubi.turntable.R
+import net.yeoubi.turntable.databinding.FragmentSearchBinding
+import net.yeoubi.turntable.view.adapter.ItemChangeListener
+import net.yeoubi.turntable.view.adapter.RecyclerAdapter
 import net.yeoubi.turntable.view.common.AttachedView
 import net.yeoubi.turntable.view.common.ViewModelFragment
+import net.yeoubi.turntable.viewmodel.SearchViewModel
 import net.yeoubi.turntable.viewmodel.common.ViewModel
+import net.yeoubi.turntable.viewmodel.item.MusicItemViewModel
+import java.util.concurrent.TimeUnit
 
 class SearchFragment : ViewModelFragment() {
 
+    lateinit var viewModel: SearchViewModel
+    lateinit var binding: FragmentSearchBinding
+    private lateinit var musicAdapter: RecyclerAdapter
+
     override fun createViewModel(context: Context, activity: AttachedView): ViewModel? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        viewModel = SearchViewModel(context, activity)
+        return viewModel
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_search, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+        binding.view = this
+        binding.viewModel = viewModel
+
+        setRecyclerView()
+        setEditText()
+
+        return binding.root
+    }
+
+    fun showBackButton(): Boolean {
+        return activity is SearchActivity
+    }
+
+    private fun setRecyclerView() {
+        musicAdapter = RecyclerAdapter(activity)
+        binding.listMusic.apply {
+            adapter = musicAdapter
+            setHasFixedSize(true)
+        }
+        viewModel.musics.addOnListChangedCallback(ItemChangeListener {
+            musicAdapter.reset(it.map(::MusicItemViewModel), R.layout.item_music)
+        })
+    }
+
+    private fun setEditText() {
+        RxTextView
+            .textChanges(binding.etInput)
+            .debounce(1000, TimeUnit.MILLISECONDS)
+            .filter { !it.isEmpty() }
+            .subscribe { viewModel.search(it.toString()) }
+            .apply { disposables.add(this) }
     }
 }
