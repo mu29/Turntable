1df9d1fdadec85252ebfc32d410b4ed91b094f37
InJung Chung <mu29@yeoubi.net>
2017-11-09 14:43:11 +0900

    음악 목록에서 아이템을 선택하면 MusicActivity 로 이동시킨다

--- app/src/main/java/net/yeoubi/turntable/data/Music.kt
+++ app/src/main/java/net/yeoubi/turntable/data/Music.kt
@@ -10,6 +10,7 @@ class Music {
     @SerializedName("id") lateinit var identifier: Identifier
     @SerializedName("snippet") lateinit var info: Info
 
+    val id get() = identifier.videoId
     val valid get() = info.title.matches(KY_REGEX)
     val artist get() = KY_REGEX.matchEntire(info.title)?.groups?.get(1)?.value
     val title get() = KY_REGEX.matchEntire(info.title)?.groups?.get(2)?.value
--- app/src/main/java/net/yeoubi/turntable/viewmodel/item/MusicItemViewModel.kt
+++ app/src/main/java/net/yeoubi/turntable/viewmodel/item/MusicItemViewModel.kt
@@ -1,10 +1,20 @@
 package net.yeoubi.turntable.viewmodel.item
 
+import net.yeoubi.turntable.common.constants.Extras
 import net.yeoubi.turntable.data.Music
+import net.yeoubi.turntable.view.MusicActivity
 import net.yeoubi.turntable.viewmodel.common.ItemViewModel
 
 /**
  * @author InJung Chung
  */
 
-class MusicItemViewModel(item: Music) : ItemViewModel<Music>(item)
+class MusicItemViewModel(item: Music) : ItemViewModel<Music>(item) {
+
+    fun onClick() {
+        activity?.startActivity(
+            MusicActivity::class,
+            Extras.ID, item.id
+        )
+    }
+}
